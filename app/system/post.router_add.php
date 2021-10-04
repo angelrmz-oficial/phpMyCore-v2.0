ddcbb1900d3aed2a67d54d50471c1956801eb420
Angel Ramirez <contacto.angelrmz@gmail.com>
2021-10-03 22:27:36 -0500

    update router_add.php

--- app/system/post.router_add.php
+++ app/system/post.router_add.php
@@ -1,10 +1,9 @@
 <?php require 'post.logged.php';
 if(!in_array("router", $userApp['permissions']))
   die(json_encode(array("success" => false, "message" => "No tiene permiso necesario para realizar esta acción", "errorCode" => "unauthorized")));
-/*if(!inputValid($post['route'], 'username'))
-    die(json_encode(array("success" => false, "message" => "Ingresa una ruta válido", "errorCode" => "InputValidError")));
-/*elseif(!inputValid($post['subtitle'], 'username'))
-    die(json_encode(array("success" => false, "message" => "Ingresa una nombre de página válido", "errorCode" => "InputValidError")));*/
+elseif(strpos($post['route'], '/') !== false && $slash = explode('/', $post['route']))
+  if(count($slash) > 2 || !inputValid($slash[0], 'username') || !inputValid($slash[1], 'username'))
+      die(json_encode(array("success" => false, "message" => "Ingresa una ruta válido", "errorCode" => "InputValidError")));
 elseif(!(new app)->CheckRouteName($post['route'])) //id duplicados ??
     die(json_encode(array("success" => false, "message" => "Este nombre de ruta ya esta siendo ocupada", "errorCode" => "InputValidError")));
 elseif(!(new app)->addroute($post))
